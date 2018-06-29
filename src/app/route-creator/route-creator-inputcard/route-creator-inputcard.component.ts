eab6cedba05a4f076b5b4a84db8d6a5a6930d772
SYDOW <marilena.sydow@lhind.dlh.de>
2018-06-29 17:27:12 +0200

    add Formgroup

--- src/app/route-creator/route-creator-inputcard/route-creator-inputcard.component.ts
+++ src/app/route-creator/route-creator-inputcard/route-creator-inputcard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { StarRatingColor } from '../../star-rating/star-rating.component';
 
 @Component({
@@ -8,22 +8,49 @@ import { StarRatingColor } from '../../star-rating/star-rating.component';
   styleUrls: ['./route-creator-inputcard.component.css']
 })
 export class RouteCreatorInputcardComponent implements OnInit {
-  activity = new FormControl();
-  activityList: string[] = ['Hiking', 'Cycling', 'Running', 'Skating', 'Mountaineering', 'Tomato'];
+  type = new FormControl();
+  typeList: string[] = ['Hiking', 'Cycling', 'Running', 'Skating', 'Mountaineering'];
+  form: FormGroup;
 
   rating = 1;
   starCount = 5;
+  ratingtype = 'difficulity';
   starColor: StarRatingColor = StarRatingColor.accent;
   starColorP: StarRatingColor = StarRatingColor.primary;
   starColorW: StarRatingColor = StarRatingColor.warn;
+  ratingElements = ['easy-going', 'exhausting', 'challenging', 'extreme', 'super extreme'];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      //[initialValue, [syncValidator], [asyncValidator]]
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      country: ['', [Validators.required, Validators.minLength(3)]],
+      place: ['', [Validators.required, Validators.minLength(3)]],
+      distance: [null, [Validators.required]],
+      type: this.fb.array(['']),
+      difficulty: ['', [Validators.required]]
+    });
   }
 
   onRatingChanged(rating) {
     this.rating = rating;
+    const controlRating = this.form.get('difficulty');
+    controlRating.setValue(this.ratingElements[(rating - 1)]);
   }
 
+  changeType(a) {
+    const typeArray: FormArray = a.value;
+    const control = <FormArray>this.form.get('type');
+    control.removeAt(0);
+    control.push(new FormControl(typeArray));
+
+  }
+
+  save(newRoute: any) {
+    console.log(newRoute);
+    // this.contactsService.addContact(contact)
+    //   .subscribe(() => this.router.navigate(['/']));
+  }
 }
