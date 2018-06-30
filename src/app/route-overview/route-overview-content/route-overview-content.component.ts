2eff5baaae2f7549ccccf01e9a0e8b69725b3f0e
Daniel Oldenburg <daniel.oldenburg@lhind.dlh.de>
2018-06-30 12:40:36 +0200

    Mocadata and improvements.

--- src/app/route-overview/route-overview-content/route-overview-content.component.ts
+++ src/app/route-overview/route-overview-content/route-overview-content.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Route } from '../../models/route';
+import { COUNTRIES_DATA } from '../../data/countries-data';
+import { count } from 'rxjs/operators';
+import { StarRatingColor } from '../../star-rating/star-rating.component';
 
 @Component({
   selector: 'trm-route-overview-content',
@@ -10,15 +13,35 @@ import { Route } from '../../models/route';
 export class RouteOverviewContentComponent implements OnInit {
 
   @Input() routesArray$: Observable<Array<Route>>;
-
+  countryData = COUNTRIES_DATA;
+  code = 'be';
   routes: Array<Route>;
+
+  rating;
+  starCount = 5;
+  ratingtypeDificulty = 'difficulity';
+  ratingtypeStar = 'star';
+  starColor: StarRatingColor = StarRatingColor.accent;
+  starColorP: StarRatingColor = StarRatingColor.primary;
+  starColorW: StarRatingColor = StarRatingColor.warn;
+  ratingElements = ['easy-going', 'exhausting', 'challenging', 'extreme', 'super extreme'];
   constructor() { }
 
   ngOnInit() {
-    this.routesArray$.subscribe(routes => {  this.routes = routes; });
+    this.routesArray$.subscribe(routes => { this.routes = routes; });
   }
 
   trackByRouteId(index, contact) {
     return contact.id;
+  }
+
+  getCountryCode(country: string) {
+    const data = this.countryData.filter(c => c.name === country).map(c2 => c2.alpha2Code);
+    return data[0] || '';
+  }
+
+  getStars(difficulty: string) {
+    return this.ratingElements.indexOf(difficulty) + 1;
+
   }
 }
